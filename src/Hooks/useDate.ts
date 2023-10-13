2d4b233d352a3842a52931faa55a1d86d803f570
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-10-13 11:06:37 -0300

    FEAT: Implementado custon hook para ter acesso a data e renderizar no Header

---
+++ src/Hooks/useDate.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import { daysOfWeek } from '../data/daysOfWeek';
+import { months } from '../data/months';
+
+const useDate = () => {
+  const [date, setDate] = useState('');
+
+  const newDate = new Date();
+
+  const getDate = () => {
+    const day = newDate.getDate();
+    const dayWeek = newDate.getDay();
+    const month = newDate.getMonth();
+    const year = newDate.getFullYear();
+
+    return `${daysOfWeek[dayWeek - 4]}/${day}/${months[month]}/${year}`;
+  };
+
+  useEffect(() => {
+    setDate(getDate());
+  }, []);
+  return { date };
+};
+
+export default useDate;
--- src/components/header/Hearder.tsx
+++ src/components/header/Hearder.tsx
@@ -1,8 +1,13 @@
+import useDate from '../../Hooks/useDate';
 import styles from './header.module.css';
 
 export default function Hearder() {
+  const { date } = useDate();
   return (
     <header>
+      <section>
+        <p>{date}</p>
+      </section>
       <section>
         <h1 className={ styles.title }>IBGE Journal</h1>
       </section>
