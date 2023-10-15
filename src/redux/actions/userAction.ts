59dd41165b1fa47ed903964a2ed158c2093f6d14
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-10-15 08:15:02 -0300

    FEAT:Implementado estado global na chave user para as notícias Favoritadas

--- src/redux/actions/userAction.ts
+++ src/redux/actions/userAction.ts
@@ -1,8 +1,28 @@
+import { NewsType } from '../../types';
+
 export const UPDATE_USER = 'UPDATE_USER';
+
+export const ADD_FAVORITES = 'ADD_FAVORITES';
+
+export const REMOVE_FAVORITES = 'ADD_FAVORITES';
 
 export const userUpdateAction = (profile:{ user:string, email:string }) => {
   return {
     type: UPDATE_USER,
     payload: profile,
+  };
+};
+
+export const addFavoritesAction = (news:NewsType) => {
+  return {
+    type: ADD_FAVORITES,
+    payload: news,
+  };
+};
+
+export const removeFavoritesAction = (news:NewsType) => {
+  return {
+    type: REMOVE_FAVORITES,
+    payload: news,
   };
 };
--- src/redux/reducers/userReducer.ts
+++ src/redux/reducers/userReducer.ts
@@ -1,16 +1,19 @@
-import { UPDATE_USER } from '../actions/userAction';
+import { NewsType } from '../../types';
+import { ADD_FAVORITES, REMOVE_FAVORITES, UPDATE_USER } from '../actions/userAction';
 
 type ActionType = {
   type:string,
   payload: {
     user:string,
     email:string,
+    favorites:NewsType[]
   }
 };
 
 const INITIAL_REDUCER = {
   user: '',
   email: '',
+  favorites: [],
 };
 
 const userReducer = (state = INITIAL_REDUCER, action:ActionType) => {
@@ -21,6 +24,19 @@ const userReducer = (state = INITIAL_REDUCER, action:ActionType) => {
         user: action.payload.user,
         email: action.payload.email,
       };
+    case ADD_FAVORITES:
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload.favorites],
+      };
+    case REMOVE_FAVORITES: {
+      const newArray = state.favorites
+        .filter((news) => news !== action.payload.favorites);
+      return {
+        ...state,
+        favorites: [...newArray],
+      };
+    }
     default:
       return state;
   }
