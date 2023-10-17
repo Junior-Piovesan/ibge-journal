46c74f26873a368f745ece0b4322693dd9e7b269
Junior Piovesan Silva <juniorpiovesan3022@outlook.com>
2023-10-17 15:22:36 -0300

    Resolvido conflitos2

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Hearder from '../../components/header/Hearder';
 import Filters from '../../components/Filters/Filters';
-import Footer from '../../components/footer/Footer';
 
 export default function Layout() {
   return (
